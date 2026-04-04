c5bbcf9f8c11e8aa47cc20a37a5755c51b65b97d
Sakshi Singh <sukku0996@gmail.com>
2026-04-05 01:18:24 +0530

    Time: 27 ms (32.72%), Space: 19.6 MB (82.94%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,20 +1,24 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        lis_mag=[0]*27
-        lis_ran=[0]*27
-        for ch in ransomNote:
-            ascii_val=ord(ch)
+        lis_mag=[0]*26
+       
+        for i in range(len(magazine)):
+            ascii_val=ord(magazine[i])
             index=ascii_val-97
-            lis_ran[index]+=1
+            lis_mag[index]+=1
 
-        for ch in magazine:
-            ascii_val=ord(ch)
+            
+
+        for i in range(len(ransomNote)):
+            ascii_val=ord(ransomNote[i])
             index=ascii_val-97
-            lis_mag[index]+=1    
+            lis_mag[index]-=1    
+             
 
-        for i in range(len(lis_ran)):
-            if lis_ran[i]>lis_mag[i]:
-                return False   
-        return True        
+        for num in lis_mag:
+            if num<0:
+                return False
+
+        return True              
             
         
