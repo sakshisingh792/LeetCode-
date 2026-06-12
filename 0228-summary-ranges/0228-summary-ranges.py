5138313eb188ec24472721d21c243ba019886fb9
Sakshi Singh <sukku0996@gmail.com>
2026-06-12 17:38:17 +0530

    Time: 0 ms (100%), Space: 19.3 MB (63.87%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -0,0 +1,23 @@
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        ans=[]
+        left=0
+        right=0
+        while left<=right and right<=len(nums)-1:
+            while right + 1 < len(nums) and nums[right+1] == nums[right] + 1:
+                right+=1
+
+            if right!=left:
+                strn=str(nums[left])+"->"+str(nums[right])
+                ans.append(strn)
+            else:
+                strn=str(nums[left])
+                ans.append(strn)
+            right+=1    
+            left=right
+        return ans                
+
+
+
+
+        
