6e12162113f5c0d53f8b17e56364db5b8152befc
Sakshi Singh <sukku0996@gmail.com>
2026-06-13 11:24:59 +0530

    Time: 0 ms (100%), Space: 23.8 MB (47.55%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
@@ -1,25 +1,27 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def countNodes(self, root: Optional[TreeNode]) -> int:
+
+        def leftHeight(node):
+            h = 0
+            while node:
+                h += 1
+                node = node.left
+            return h
+
+        def rightHeight(node):
+            h = 0
+            while node:
+                h += 1
+                node = node.right
+            return h
+
         if not root:
             return 0
-        from collections import deque
-        q=deque([root])
-        count=0
-        while q:
-            n=len(q)
-            for i in range(n):
-                
-                node=q.popleft()
-                count+=1
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
-                         
-        return count 
+
+        lh = leftHeight(root)
+        rh = rightHeight(root)
+
+        if lh == rh:
+            return (1 << lh) - 1
+
+        return 1 + self.countNodes(root.left) + self.countNodes(root.right)
