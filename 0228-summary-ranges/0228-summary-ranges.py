a284be8b98b634f166af4746576b26d2ffe11711
Sakshi Singh <sukku0996@gmail.com>
2026-06-24 00:30:59 +0530

    Time: 0 ms (100%), Space: 19.3 MB (23.84%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.py
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -3,21 +3,19 @@ class Solution:
         ans=[]
         left=0
         right=0
-        while left<=right and right<=len(nums)-1:
-            while right + 1 < len(nums) and nums[right+1] == nums[right] + 1:
+        while left<=right and right<len(nums):
+            while right+1<len(nums) and nums[right+1]==nums[right]+1:
                 right+=1
-
-            if right!=left:
+            if left!=right:
                 strn=str(nums[left])+"->"+str(nums[right])
                 ans.append(strn)
             else:
                 strn=str(nums[left])
-                ans.append(strn)
-            right+=1    
-            left=right
-        return ans                
+                ans.append(strn)   
+            right+=1
+            left=right    
+        return ans     
+                    
 
 
-
-
-        
+            
