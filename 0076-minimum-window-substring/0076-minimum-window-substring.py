73c7106efc9c3d533953ea14d62166a83293f192
Sakshi Singh <sukku0996@gmail.com>
2026-06-23 19:08:50 +0530

    Time: 55 ms (91.91%), Space: 19.5 MB (93.95%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,34 +1,36 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         target={}
-        for ch in t:
+        for i in range(len(t)):
+            ch=t[i]
             target[ch]=target.get(ch,0)+1
-
 
         have=0
         need=len(target)
         ans=[-1,-1]
         ans_len=float("inf")
-        l=0
+
         freq={}
+        l=0
         for r in range(len(s)):
             ch=s[r]
             freq[ch]=freq.get(ch,0)+1
+            if  ch in target and freq[ch]==target[ch]:
+                have+=1    
 
-            if ch in target and freq[ch]==target[ch]:
-                have+=1
+                while have==need:
+                    if r-l+1<ans_len:
+                        ans=[l,r]
+                        ans_len=r-l+1
 
+                    freq[s[l]]-=1
+                    if s[l] in target and target[s[l]]>freq[s[l]]:
+                        have-=1
 
-            while have==need:
-                if r-l+1<ans_len:
-                    ans=[l,r] 
-                    ans_len=r-l+1
-
-                freq[s[l]]-=1
-                if s[l] in target and target[s[l]]>freq[s[l]]:
-                    have-=1
-
-                l+=1
+                    l+=1
         l,r=ans
+        return s[l:r+1] if  ans_len!=float("inf") else   ""       
 
-        return s[l:r+1] if ans_len!=float ("inf") else ""                   
+
+
+        
