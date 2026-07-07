27871f1e0df84a58451e1b9c2ca7dad68039fe85
Sakshi Singh <sukku0996@gmail.com>
2026-07-07 17:28:55 +0530

    Time: 0 ms (100%), Space: 19.5 MB (16.94%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -1,21 +1,17 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        memo={}
-        def dfs(n):
-            if n==0:
-                return 0
-            if n==1:
-                return 1
-            if n==2:
-                return 1
+        dp=[0]*(n+1)
 
+        if n==0:
+            return 0
+        if n==1 or n==2:
+            return 1
 
-            if n in memo:
-                return memo[n]
-            res=dfs(n-1)+dfs(n-2)+dfs(n-3)
-            memo[n]=res
-            return memo[n]
-        return dfs(n)    
+        dp[0]=0
+        dp[1]=1
+        dp[2]=1
+        for i in range(3,n+1):
+            dp[i]=dp[i-1]+ dp[i-2]+dp[i-3]
 
-
-        
+        return dp[n]            
+       
