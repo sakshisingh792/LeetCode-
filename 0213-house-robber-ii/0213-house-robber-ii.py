ef52bfa52f05b752ec3817a053ef301cef5b9df9
Sakshi Singh <sukku0996@gmail.com>
2026-08-20 18:56:18 +0530

    Time: 0 ms (100%), Space: 19 MB (99.26%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,27 +1,39 @@
 class Solution:
+
+    def solve(self,nums,dp,ind):
+        if ind==0:
+            return nums[0]
+
+        if dp[ind]!=-1:
+            return dp[ind]
+
+        if ind>1:
+          pick=nums[ind]+self.solve(nums,dp,ind-2)
+        else:
+            pick=nums[ind]   
+        skip=self.solve(nums,dp,ind-1)
+
+        dp[ind]=max(pick,skip)
+
+        return dp[ind]        
     def rob(self, nums: List[int]) -> int:
         n=len(nums)
-        if n == 1:
-          return nums[0]
 
-        def dfs(start,end):
-            dp=[-1]*n
+        if n==1:
+            return nums[0]
 
-            def solve(i):
-                if i>end:
-                    return 0
+        arr1=nums[0:n-1]
+        dp1=[-1]*len(arr1) 
 
+        arr2=nums[1:n]
+        dp2=[-1]*len(arr2)
 
-                if dp[i]!=-1:
-                    return dp[i]
+        ans1=self.solve(arr1,dp1,len(arr1)-1)   
+        ans2=self.solve(arr2,dp2,len(arr2)-1)
 
+    
 
-                rob=nums[i]+solve(i+2)
-                skip=solve(i+1)
-                dp[i]=max(rob,skip)
-                return dp[i]
-            return solve(start)
-        return max(dfs(0,n-2),dfs(1,n-1))        
+        return max(ans1,ans2)
 
-
-       
+                 
+        
