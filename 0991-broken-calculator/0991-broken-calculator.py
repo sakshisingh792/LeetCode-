bff704742aa9e3ff9614699fd8954bd6d0c899aa
Sakshi Singh <sukku0996@gmail.com>
2026-08-22 15:02:56 +0530

    Time: 0 ms (100%), Space: 19.3 MB (34.8%) - LeetHub

--- 0991-broken-calculator/0991-broken-calculator.py
+++ 0991-broken-calculator/0991-broken-calculator.py
@@ -1,6 +1,8 @@
 class Solution:
     def brokenCalc(self, startValue: int, target: int) -> int:
         count = 0
+        if startValue==target:
+            return 0
 
         while target > startValue:
             if target % 2 == 0:
