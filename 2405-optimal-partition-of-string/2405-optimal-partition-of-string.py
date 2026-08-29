a9cebf80e9296a129456a3705087741e1982d4ff
Sakshi Singh <sukku0996@gmail.com>
2026-08-29 15:47:45 +0530

    Time: 63 ms (35.96%), Space: 19.5 MB (98.03%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
@@ -1,11 +1,11 @@
 class Solution:
     def partitionString(self, s: str) -> int:
         ans=1
-        freq={}
+        seen=set()
         for i in range(len(s)):
             ch=s[i]
-            if s[i] in freq:
+            if s[i] in seen:
                 ans+=1
-                freq={}
-            freq[ch]=freq.get(ch,0)+1
+                seen=set()
+            seen.add(ch)
         return ans        
